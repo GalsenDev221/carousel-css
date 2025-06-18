5d1c03cfda772ab8ba0343466de19c249cf1043b
NDOY3M4N <pa.ndoye@outlook.fr>
2025-06-18 10:26:58 +0000

    chore: setup the stage

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <ul class="carousel">
+      <li v-for="i in 10" :key="i" class="carousel__item">{{ i }}</li>
+    </ul>
+  </main>
+</template>
+
+<style scoped>
+.carousel {
+  width: 500px;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  .carousel__item {
+    width: 250px;
+    aspect-ratio: 3/4;
+    background-color: hsl(0 0% 20%);
+    border: 1px solid hsl(0 0% 30%);
+    border-radius: 6px;
+    flex-shrink: 0;
+    font-size: 4rem;
+    font-weight: 800;
+    display: grid;
+    place-content: center;
+  }
+}
+</style>
