e22b4abd3335a04d8391eb194fa6f1b4efbc9aa8
NDOY3M4N <pa.ndoye@outlook.fr>
2025-06-19 08:55:29 +0000

    feat: add carousel buttons

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,53 @@
-<script lang="ts" setup>
-import { onMounted, reactive, ref, watch } from 'vue'
-
-type CarouselProps = {
-  width: number
-  itemCount: number
-  scrollStep: number
-}
-
-const carousel: CarouselProps = reactive({
-  width: 0,
-  itemCount: 10,
-  scrollStep: 300,
-})
-
-const carouselContainer = ref<HTMLUListElement | null>(null)
-const currIdx = ref(0)
-
-function handleNext() {
-  currIdx.value++
-}
-
-function handlePrev() {
-  currIdx.value--
-}
-
-onMounted(() => {
-  if (carouselContainer.value) {
-    carousel.width = carouselContainer.value.offsetWidth
-  }
-})
-
-watch(currIdx, () => {
-  if (carouselContainer.value) {
-    const translateX = Math.max(
-      0,
-      Math.min(
-        currIdx.value * carousel.scrollStep,
-        carouselContainer.value.scrollWidth - carousel.width,
-      ),
-    )
-    carouselContainer.value.style.transform = `translateX(-${translateX}px)`
-  }
-})
-</script>
-
 <template>
   <main>
-    <div class="carousel-wrapper">
-      <ul ref="carouselContainer" class="carousel">
-        <li v-for="i in carousel.itemCount" :key="i" class="carousel__item">{{ i }}</li>
-      </ul>
-    </div>
-
-    <footer>
-      <button @click="handlePrev">Prev</button>
-      <button @click="handleNext">Next</button>
-    </footer>
+    <ul ref="carouselContainer" class="carousel">
+      <li v-for="i in 10" :key="i" class="carousel__item">{{ i }}</li>
+    </ul>
   </main>
 </template>
 
 <style scoped>
-.carousel-wrapper {
+.carousel {
   overflow-x: auto;
   scrollbar-width: none;
   scroll-snap-type: x mandatory;
-}
+  scroll-behavior: smooth;
 
-.carousel {
   width: 500px;
   display: flex;
   align-items: center;
   gap: 1rem;
-  transition: transform 0.5s ease;
+  position: relative;
+  anchor-name: --carousel;
 
+  /* NOTE: carousel buttons */
+  &::scroll-button(*) {
+    position: fixed;
+    position-anchor: --carousel;
+    width: 64px;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    margin: 2rem;
+    background-color: transparent;
+    border: 3px solid hsl(0 0% 30%);
+    cursor: pointer;
+  }
+
+  &::scroll-button(*):disabled {
+    cursor: not-allowed;
+  }
+
+  &::scroll-button(left) {
+    content: '👈' / 'Defilez a gauche';
+    position-area: inline-start center;
+  }
+
+  &::scroll-button(right) {
+    content: '👉' / 'Defilez a droite';
+    position-area: inline-end center;
+  }
+
+  /* NOTE: carousel items */
   .carousel__item {
     scroll-snap-align: center;
     width: 250px;
@@ -85,20 +60,6 @@ watch(currIdx, () => {
     font-weight: 800;
     display: grid;
     place-content: center;
-  }
-}
-
-footer {
-  margin-block-start: 2rem;
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-
-  button {
-    background-color: hsl(0 0% 30%);
-    padding: 1rem 2rem;
-    font-size: 1.3rem;
-    color: white;
   }
 }
 </style>
